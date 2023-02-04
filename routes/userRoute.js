63b920a7fdcf0ef7cb69ab68d24305552ad08030
Gottardy <gottgab@gmail.com>
2023-02-04 18:56:48 -0500

    se realiza la modificacion del metodo que valida que el correo existe previamente en la BD

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
 const {usersGet, usersPut,  usersPost, usersDelete} = require('../controllers/usersController');
-const { esUnRolValido } = require('../DB/validators.db');
+const { esUnRolValido, existeCorreo } = require('../DB/validators.db');
 
 
 const { validarParametros } = require('../middlewares/validarCampos');
@@ -16,6 +16,7 @@ router.post('/', [
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('password','El password es obligatorio y debe ser más de 8 letras').isLength({min:8}),
     check('correo','El correo enviado no es valido').isEmail(),
+    check('correo').custom( existeCorreo ),
     // check('rol','El rol enviado no es valido').isIn(['ADMIN_ROL','USER_ROL']),
     check('rol').custom( esUnRolValido ),
     validarParametros
