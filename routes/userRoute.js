9766c20a93f9d1fd6a9d80cc69c5a2b317bb69cd
Gottardy <gottgab@gmail.com>
2023-02-04 18:13:44 -0500

    se actualiza el archivo de rutas con la validacion del rol que se esta enviando a la BD

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
 const {usersGet, usersPut,  usersPost, usersDelete} = require('../controllers/usersController');
+const Role = require('../models/role');
+
 const { validarParametros } = require('../middlewares/validarCampos');
 
 const router = Router();
@@ -13,7 +15,13 @@ router.post('/', [
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('password','El password es obligatorio y debe ser más de 8 letras').isLength({min:8}),
     check('correo','El correo enviado no es valido').isEmail(),
-    check('rol','El rol enviado no es valido').isIn(['ADMIN_ROL','USER_ROL']),
+    // check('rol','El rol enviado no es valido').isIn(['ADMIN_ROL','USER_ROL']),
+    check('rol').custom( async (rol ='')=>{
+        const existeRol = await Role.findOne({ rol });
+        if( !existeRol){
+            throw new Error (`El rol ${rol} no esta registrado en la BD`);
+        }
+    }),
     validarParametros
 ], usersPost );
 
