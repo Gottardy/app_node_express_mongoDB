13f6463dd82e71584c1e32e41690054b2d7ff254
Gottardy <gottgab@gmail.com>
2023-02-04 18:56:03 -0500

    se crea el metodo existeCorreo para la validacion previa del registro unico en la bd y no se repita

--- DB/validators.db.js
+++ DB/validators.db.js
@@ -1,12 +1,24 @@
 const Role = require('../models/role');
+const usuario = require('../models/usuario');
 
-const  esUnRolValido = async (rol = '')=>{
-    const existeRol = await Role.findOne({ rol });
-    if( !existeRol){
-        throw new Error (`El rol ${rol} no esta registrado en la BD`);
+    //Verificar si el rol existe
+    const  esUnRolValido = async (rol = '')=>{
+        const existeRol = await Role.findOne({ rol });
+        if( !existeRol){
+            throw new Error (`El rol ${rol} no esta registrado en la BD`);
+        }
     }
-}
+
+    //Verificar si el correo existe
+    const existeCorreo = async (correo) =>{
+        const correoPresent = await usuario.findOne({correo});
+        if (correoPresent){
+            throw new Error (`El correo {${correo}} enviado ya esta registrado, por favor cambielo`);
+         }
+   }
+    
 
 module.exports = {
     esUnRolValido,
+    existeCorreo
 }
