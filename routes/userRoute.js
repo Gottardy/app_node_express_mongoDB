970522800e941f0049f851503d792684e8fe712b
Gottardy <gottgab@gmail.com>
2023-02-04 17:08:15 -0500

    Agregando validaciones adicionales a la ruta del metodo POST

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,7 +9,10 @@ router.get('/', usersGet );
 router.put('/:id', usersPut );
 
 router.post('/', [
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','El password es obligatorio').isLength({min:8}),
     check('correo','El correo enviado no es valido').isEmail(),
+    check('rol','El rol enviado no es valido').not().isIn(['ADMIN_ROL','USER_ROL']),
 ], usersPost );
 
 router.delete('/', usersDelete );
