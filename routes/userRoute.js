e310163ed55638b241bef0fa12a9dddc39221c10
Gottardy <gottgab@gmail.com>
2023-02-04 18:33:09 -0500

    refactorizamos en una funion la validacion del rol que se ingresa

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
 const {usersGet, usersPut,  usersPost, usersDelete} = require('../controllers/usersController');
-const Role = require('../models/role');
+const { esUnRolValido } = require('../DB/validators.db');
+
 
 const { validarParametros } = require('../middlewares/validarCampos');
 
@@ -16,12 +17,7 @@ router.post('/', [
     check('password','El password es obligatorio y debe ser más de 8 letras').isLength({min:8}),
     check('correo','El correo enviado no es valido').isEmail(),
     // check('rol','El rol enviado no es valido').isIn(['ADMIN_ROL','USER_ROL']),
-    check('rol').custom( async (rol ='')=>{
-        const existeRol = await Role.findOne({ rol });
-        if( !existeRol){
-            throw new Error (`El rol ${rol} no esta registrado en la BD`);
-        }
-    }),
+    check('rol').custom( esUnRolValido ),
     validarParametros
 ], usersPost );
 
